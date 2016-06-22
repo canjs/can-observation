8e5e8d8ba6135322bbc0a8669cedc80270b461c1
Matthew Phillips <matthew@matthewphillips.info>
2016-06-22 14:36:41 -0400

    3.0.0-pre.2

---
+++ dist/amd/can-observation.js
@@ -0,0 +1,234 @@
+/*can-observation@3.0.0-pre.1#can-observation*/
+define(function (require, exports, module) {
+    require('can-event');
+    var canBatch = require('can-event/batch');
+    var assign = require('can-util/js/assign');
+    var namespace = require('can-util/namespace');
+    function Observation(func, context, compute) {
+        this.newObserved = {};
+        this.oldObserved = null;
+        this.func = func;
+        this.context = context;
+        this.compute = compute.updater ? compute : { updater: compute };
+        this.onDependencyChange = this.onDependencyChange.bind(this);
+        this.depth = null;
+        this.childDepths = {};
+        this.ignore = 0;
+        this.inBatch = false;
+        this.ready = false;
+        compute.observedInfo = this;
+        this.setReady = this._setReady.bind(this);
+    }
+    var observationStack = [];
+    assign(Observation.prototype, {
+        getPrimaryDepth: function () {
+            return this.compute._primaryDepth || 0;
+        },
+        _setReady: function () {
+            this.ready = true;
+        },
+        getDepth: function () {
+            if (this.depth !== null) {
+                return this.depth;
+            } else {
+                return this.depth = this._getDepth();
+            }
+        },
+        _getDepth: function () {
+            var max = 0, childDepths = this.childDepths;
+            for (var cid in childDepths) {
+                if (childDepths[cid] > max) {
+                    max = childDepths[cid];
+                }
+            }
+            return max + 1;
+        },
+        addEdge: function (objEv) {
+            objEv.obj.addEventListener(objEv.event, this.onDependencyChange);
+            if (objEv.obj.observedInfo) {
+                this.childDepths[objEv.obj._cid] = objEv.obj.observedInfo.getDepth();
+                this.depth = null;
+            }
+        },
+        removeEdge: function (objEv) {
+            objEv.obj.removeEventListener(objEv.event, this.onDependencyChange);
+            if (objEv.obj.observedInfo) {
+                delete this.childDepths[objEv.obj._cid];
+                this.depth = null;
+            }
+        },
+        dependencyChange: function (ev) {
+            if (this.bound && this.ready) {
+                if (ev.batchNum !== undefined) {
+                    if (ev.batchNum !== this.batchNum) {
+                        Observation.registerUpdate(this);
+                        this.batchNum = ev.batchNum;
+                    }
+                } else {
+                    this.updateCompute(ev.batchNum);
+                }
+            }
+        },
+        onDependencyChange: function (ev, newVal, oldVal) {
+            this.dependencyChange(ev, newVal, oldVal);
+        },
+        updateCompute: function (batchNum) {
+            if (this.bound) {
+                var oldValue = this.value;
+                this.getValueAndBind();
+                this.compute.updater(this.value, oldValue, batchNum);
+            }
+        },
+        getValueAndBind: function () {
+            this.bound = true;
+            this.oldObserved = this.newObserved || {};
+            this.ignore = 0;
+            this.newObserved = {};
+            this.ready = false;
+            observationStack.push(this);
+            this.value = this.func.call(this.context);
+            observationStack.pop();
+            this.updateBindings();
+            canBatch.afterPreviousEvents(this.setReady);
+        },
+        updateBindings: function () {
+            var newObserved = this.newObserved, oldObserved = this.oldObserved, name, obEv;
+            for (name in newObserved) {
+                obEv = newObserved[name];
+                if (!oldObserved[name]) {
+                    this.addEdge(obEv);
+                } else {
+                    oldObserved[name] = null;
+                }
+            }
+            for (name in oldObserved) {
+                obEv = oldObserved[name];
+                if (obEv) {
+                    this.removeEdge(obEv);
+                }
+            }
+        },
+        teardown: function () {
+            this.bound = false;
+            for (var name in this.newObserved) {
+                var ob = this.newObserved[name];
+                this.removeEdge(ob);
+            }
+            this.newObserved = {};
+        }
+    });
+    var updateOrder = [], curPrimaryDepth = Infinity, maxPrimaryDepth = 0;
+    Observation.registerUpdate = function (observeInfo, batchNum) {
+        var depth = observeInfo.getDepth() - 1;
+        var primaryDepth = observeInfo.getPrimaryDepth();
+        curPrimaryDepth = Math.min(primaryDepth, curPrimaryDepth);
+        maxPrimaryDepth = Math.max(primaryDepth, maxPrimaryDepth);
+        var primary = updateOrder[primaryDepth] || (updateOrder[primaryDepth] = {
+            observeInfos: [],
+            current: Infinity,
+            max: 0
+        });
+        var objs = primary.observeInfos[depth] || (primary.observeInfos[depth] = []);
+        objs.push(observeInfo);
+        primary.current = Math.min(depth, primary.current);
+        primary.max = Math.max(depth, primary.max);
+    };
+    Observation.batchEnd = function (batchNum) {
+        var cur;
+        while (true) {
+            if (curPrimaryDepth <= maxPrimaryDepth) {
+                var primary = updateOrder[curPrimaryDepth];
+                if (primary && primary.current <= primary.max) {
+                    var last = primary.observeInfos[primary.current];
+                    if (last && (cur = last.pop())) {
+                        cur.updateCompute(batchNum);
+                    } else {
+                        primary.current++;
+                    }
+                } else {
+                    curPrimaryDepth++;
+                }
+            } else {
+                updateOrder = [];
+                curPrimaryDepth = Infinity;
+                maxPrimaryDepth = 0;
+                return;
+            }
+        }
+    };
+    Observation.add = function (obj, event) {
+        var top = observationStack[observationStack.length - 1];
+        if (top && !top.ignore) {
+            var evStr = event + '', name = obj._cid + '|' + evStr;
+            if (top.traps) {
+                top.traps.push({
+                    obj: obj,
+                    event: evStr,
+                    name: name
+                });
+            } else if (!top.newObserved[name]) {
+                top.newObserved[name] = {
+                    obj: obj,
+                    event: evStr
+                };
+            }
+        }
+    };
+    Observation.addAll = function (observes) {
+        var top = observationStack[observationStack.length - 1];
+        if (top) {
+            if (top.traps) {
+                top.traps.push.apply(top.traps, observes);
+            } else {
+                for (var i = 0, len = observes.length; i < len; i++) {
+                    var trap = observes[i], name = trap.name;
+                    if (!top.newObserved[name]) {
+                        top.newObserved[name] = trap;
+                    }
+                }
+            }
+        }
+    };
+    Observation.ignore = function (fn) {
+        return function () {
+            if (observationStack.length) {
+                var top = observationStack[observationStack.length - 1];
+                top.ignore++;
+                var res = fn.apply(this, arguments);
+                top.ignore--;
+                return res;
+            } else {
+                return fn.apply(this, arguments);
+            }
+        };
+    };
+    Observation.trap = function () {
+        if (observationStack.length) {
+            var top = observationStack[observationStack.length - 1];
+            var oldTraps = top.traps;
+            var traps = top.traps = [];
+            return function () {
+                top.traps = oldTraps;
+                return traps;
+            };
+        } else {
+            return function () {
+                return [];
+            };
+        }
+    };
+    Observation.trapsCount = function () {
+        if (observationStack.length) {
+            var top = observationStack[observationStack.length - 1];
+            return top.traps.length;
+        } else {
+            return 0;
+        }
+    };
+    Observation.isRecording = function () {
+        var len = observationStack.length;
+        return len && observationStack[len - 1].ignore === 0;
+    };
+    canBatch._onDispatchedEvents = Observation.batchEnd;
+    module.exports = namespace.Observation = Observation;
+});
